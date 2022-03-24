48ca0df0175806f1a83e811bff2a39f2697f96de
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-24 16:59:34 -0700

    adjust API call

--- src/services/exchange-service.js
+++ src/services/exchange-service.js
@@ -1,6 +1,6 @@
 export default class ExchangeService {
-  static getCurrency() {
-    return fetch('https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD')
+  static getCurrency(currency, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
