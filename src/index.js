071425fb0ebfa197d03148e4c1d447653a261929
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-24 16:38:42 -0700

    add initial UI logic

--- src/index.js
+++ src/index.js
@@ -2,10 +2,35 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import CLASS from './business.js';
+import ExchangeService from './services/exchange-service.js';
 
+function clearFields() {
+  $('#amount').val("");
+  $('#currency').val("");
+  $('.showRates').text("");
+  $('.showErrors').text("");
+}
 
-//UI Logic
+function getElements(response) {
+  if(response) {
+    const conversion = (response.conversion_rate * dollars);
+    
+    $('.showRates').append(`Your chosen amount of USD is worth ${conversion} in ${dollars}.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
+}
 
+$(document).ready(function() {
+  $('#submitExchange').click(function() {
+    const amount = $('#amount').val();
+    const currency = $('#currency').val();
+    clearFields();
+    ExchangeService.getCurrency(currency, amount)
+    .then(function(response) {
+      getElements(response);
+    });
+  });
+});
 
 
