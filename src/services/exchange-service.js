2c08a39eeb46b51ec77af2299d80adfe1f271a69
Aaron Boyd <a.boyd91@yahoo.com>
2022-03-25 16:19:23 -0700

    add second parameter to API call to change base currency

--- src/services/exchange-service.js
+++ src/services/exchange-service.js
@@ -1,6 +1,6 @@
 export default class ExchangeService {
-  static getCurrency(currency, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
+  static getCurrency(currencyFrom, currencyTo, amount) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${currencyFrom}/${currencyTo}/${amount}`)
     .then(function(response) {
       if (!response.ok) {
         throw Error(response.statusText);
